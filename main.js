2a6be09c2d4591ecd7bfdb0d301523c919cb03b3
Ilhom Musayev <73127814+IlhomMusayev@users.noreply.github.com>
2021-05-23 13:11:57 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -14,10 +14,15 @@ const yourNowResult = document.querySelector('.your-now-result')
 
 const computerNowResult = document.querySelector('.computer-now-result')
 
+const yourNowResultImgElement = document.querySelector('.your-result')
+
+const computerNowResultImgElement = document.querySelector('.computer-result')
+
 const restartBtn = document.querySelector('.restart-btn')
 let yourResult = 0,
     computerResult = 0;
-
+    let yourImg = document.createElement('img')
+    let computerImg = document.createElement('img')
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -27,48 +32,68 @@ function getRandomInt(min, max) {
 stonButtonElement.onclick = function () {
     your = 1
     computer = getRandomInt(1,4)
-    console.log(computer);
+    yourImg.src = 'tosh.jpeg'
+    yourNowResult.append(yourImg)
     if(your == 1 && computer == 1){
         gameResult.textContent = 'Durrang'; 
-
+        computerImg.src ='tosh.jpeg'
+        computerNowResult.append(computerImg)
     }else if(your == 1 && computer == 2){
         yourResult++
         yourResultElement.textContent = yourResult
         gameResult.textContent = 'Siz yuttingiz';
+        computerImg.src ='qaychi.jpeg'
+        computerNowResult.append(computerImg)
     }else if(your == 1 && computer == 3){
         computerResult++
         computerResultElement.textContent = computerResult
         gameResult.textContent = 'Yutqazdingiz';
+        computerImg.src ='qogoz.png'
+        computerNowResult.append(computerImg)
     }
 }
 scissorsButtonElement.onclick = function () {
     your = 2
     computer = getRandomInt(1,4)
+    yourImg.src = 'qaychi.jpeg'
     if(your == 2 && computer == 1){
         computerResult++
         computerResultElement.textContent = computerResult
         gameResult.textContent = 'Yutqazdingiz'; 
+        computerImg.src ='tosh.jpeg'
+        computerNowResult.append(computerImg)
     }else if(your == 2 && computer == 2){
         gameResult.textContent = 'Durrang';
+        computerImg.src ='qaychi.jpeg'
+        computerNowResult.append(computerImg)
     }else if(your == 2 && computer == 3){
         yourResult++
         yourResultElement.textContent = yourResult
         gameResult.textContent = 'Yutdingiz';
+        computerImg.src ='qogoz.png'
+        computerNowResult.append(computerImg)
     }
 }
 paperButtonElement.onclick = function () {
     your = 3
     computer = getRandomInt(1,4)
+    yourImg.src = 'qogoz.png'
     if(your == 3 && computer == 1){
         yourResult++
         yourResultElement.textContent = yourResult
         gameResult.textContent = 'Yutdingiz';
+        computerImg.src ='tosh.jpeg'
+        computerNowResult.append(computerImg)
     }else if(your == 3 && computer == 2){
         computerResult++
         computerResultElement.textContent = computerResult
         gameResult.textContent = 'Yutqazdingiz'; 
+        computerImg.src ='qaychi.jpeg'
+        computerNowResult.append(computerImg)
     }else if(your == 3 && computer == 3){
         gameResult.textContent = 'Durrang';
+        computerImg.src ='qogoz.png'
+        computerNowResult.append(computerImg)
     }
 }
 restartBtn.addEventListener('click', e=>{
